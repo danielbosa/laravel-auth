d186772562dff5b0c1758019a70a24e851840091
danielbosa <danielbosa95@gmail.com>
2024-06-01 16:08:51 +0200

    Migration too add column to Projects table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,16 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //$newProject = new Project();
-        //$newProject->slug = Project::generateSlug($newProject->slug);
-        //newProject->save();
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+            $newProject->title = $faker->sentence;
+            $newProject->url = $faker->url;
+            $newProject->slug = Project::generateSlug($newProject->title);
+            $newProject->save(); 
+        }
+        
     }
 }
