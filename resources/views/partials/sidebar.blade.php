0d8b141dcaf9ae5c5b8c92875bc911fe0b24c5b6
danielbosa <danielbosa95@gmail.com>
2024-06-03 07:50:49 +0200

    Fixed scrolling mobile

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,15 +7,15 @@
             </a>
         </li>
         <li class="{{Route::currentRouteName() == 'admin.dashboard' ? 'active' : ''}}">
-            <a href="/" class="nav-link">
+            <a href="/admin" class="nav-link">
                 <i class="fa-solid fa-chart-simple"></i>
                 <span>Dashboard</span>
             </a>
         </li>
         <li class="{{Route::currentRouteName() == 'admin.projects.index' ? 'active' : ''}}">
-            <a href="/" class="nav-link">
+            <a href="/admin/projects" class="nav-link">
                 <i class="fa-solid fa-signal"></i>
-                <span>Index</span>
+                <span>Projects</span>
             </a>
         </li>
     </ul>
